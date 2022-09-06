e0769b976255e253231da149cfe507078d9e6b11
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-09-06 18:48:33 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-//https://teachablemachine.withgoogle.com/models/vVjEJF50Q/
+
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/vVjEJF50Q/model.json', modelReady)    
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+console.log('Got Result');
+}
